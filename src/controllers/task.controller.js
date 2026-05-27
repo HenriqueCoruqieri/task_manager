a40b08c2a748aaf23db2fb3effcc86cc85859ff6
HenriqueCoruqieri <heoliveirac@gmail.com>
2026-05-27 00:09:15 -0300

    feat: move get task by id endpoint to a controller

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -15,6 +15,26 @@ class TaskController {
             this.res.status(500).send(error.message)
           }
     }
+
+    async getTaskById(){
+      try{
+        const taskId = this.req.params.id
+    
+        const task = await TaskModel.findById(taskId)
+    
+        if(!task){
+          return this.res.status(404).send('Essa tarefa não foi encontrada')
+        }
+    
+        return this.res.status(200).send(task)
+    
+      } catch(error){
+        console.error(error)
+        this.res.status(500).send(error.message)
+      }
+
+      
+    }
 }
 
 module.exports = TaskController
--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -9,22 +9,8 @@ router.get('/', async (req, res) => {
     return new TaskController(req, res).getTasks()
   })
   
-  router.get('/:id', async (req,res) => {
-    try{
-      const taskId = req.params.id
-  
-      const task = await TaskController.findById(taskId)
-  
-      if(!task){
-        return res.status(404).send('Essa tarefa não foi encontrada')
-      }
-  
-      return res.status(200).send(task)
-  
-    } catch(error){
-      console.error(error)
-      res.status(500).send(error.message)
-    }
+  router.get('/:id', async (req, res) => {
+    return new TaskController(req, res).getTaskById()
   })
   
   router.post('/', async (req, res) =>{
