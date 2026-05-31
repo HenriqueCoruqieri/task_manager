f2bbfcf661ae99ed859fae471d514fda89c87a90
HenriqueCoruqieri <heoliveirac@gmail.com>
2026-05-31 12:07:02 -0300

    chore: add eslint-plugin-prettier

--- eslint.config.js
+++ eslint.config.js
@@ -2,12 +2,22 @@ import js from "@eslint/js"
 import globals from "globals"
 import reactHooks from "eslint-plugin-react-hooks"
 import reactRefresh from "eslint-plugin-react-refresh"
+import eslintConfigPrettier from "eslint-config-prettier"
 import { defineConfig, globalIgnores } from "eslint/config"
 
 export default defineConfig([
-  globalIgnores(["dist"]),
+  globalIgnores(["dist", "node_modules"]),
   {
-    files: ["**/*.{js,jsx}"],
+    files: ["backend/**/*.js"],
+    languageOptions: {
+      globals: globals.node,
+      sourceType: "commonjs",
+    },
+    rules: js.configs.recommended.rules,
+  },
+
+  {
+    files: ["src/**/*.{js, jsx}"],
     extends: [
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
@@ -18,4 +28,6 @@ export default defineConfig([
       parserOptions: { ecmaFeatures: { jsx: true } },
     },
   },
+
+  eslintConfigPrettier,
 ])
