345593c52cadee5b8f28f25fe60f587264263d16
HenriqueCoruqieri <heoliveirac@gmail.com>
2026-05-31 04:07:27 -0300

    feat: add login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,51 @@
+import { UserRoundKey } from "lucide-react"
+
 function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-base-200">
-      <div className="card w-96 bg-base-100 shadow-xl">
-        <div className="card-body items-center text-center gap-4">
-          <h1 className="card-title text-3xl font-bold">Task Manager</h1>
-          <p className="text-base-content/70">
-            Hello World! O frontend está funcionando.
-          </p>
-          <div className="badge badge-primary">Vite + React + DaisyUI</div>
-          <div className="card-actions">
-            <button className="btn btn-primary">Começar</button>
-          </div>
-        </div>
+    <div className="min-h-screen flex flex-col items-center justify-center bg-base-200">
+      <div className="flex justify-center mb-12">
+        <img
+          alt="Task Manager Logo"
+          src="task_manager.png"
+          className="w-[25%] h-[25%]"
+        />
       </div>
+
+      <div className="flex flex-row space-x-2 mt-12">
+        <h2 className="font-bold">Faça seu login</h2>
+        <UserRoundKey />
+      </div>
+
+      <form>
+        <div className="flex flex-col gap-4 mt-12">
+          <input
+            type="text"
+            placeholder="Usuário"
+            className="input input-lg w-100"
+          />
+          <input
+            type="password"
+            placeholder="Senha"
+            className="input input-lg w-100"
+          />
+        </div>
+
+        <div className="flex flex-col space-y-2 mt-8">
+          <button
+            type="submit"
+            className="btn btn-success border-4 border-solid w-100 rounded-full text-white"
+          >
+            Login
+          </button>
+
+          <button
+            type="outline"
+            className="btn border-4 border-solid rounded-full"
+          >
+            Cadastrar
+          </button>
+        </div>
+      </form>
     </div>
   )
 }
