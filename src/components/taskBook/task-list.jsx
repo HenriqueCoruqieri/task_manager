d392a07a6aec3e52ea62d7d4c7e30e6f0b5d0c66
HenriqueCoruqieri <heoliveirac@gmail.com>
2026-06-08 01:20:37 -0300

    feat: show user tasks on TaskList

--- src/components/taskBook/task-list.jsx
+++ src/components/taskBook/task-list.jsx
@@ -1,6 +1,21 @@
+import { useEffect, useState } from "react"
+
 import { PencilLine } from "lucide-react"
 
 const TaskList = () => {
+  const [tasks, setTasks] = useState([])
+  const [selectedTask, setSelectedTask] = useState([])
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      const response = await fetch("http://localhost:8000/tasks")
+      const data = await response.json()
+      setTasks(data)
+    }
+
+    fetchTasks()
+  }, [])
+
   return (
     <div className="min-w-[500px]">
       <ul className="list rounded-box space-y-4">
@@ -8,125 +23,46 @@ const TaskList = () => {
           Lista de Tarefas
         </li>
 
-        <li className="flex items-center gap-4">
-          <button className="btn btn-sm border-2 border-white border-solid rounded-xl mt-2">
-            <PencilLine />
-          </button>
-          <div className="list-row bg-emerald-600">
-            <div className="text-4xl font-thin opacity-30 tabular-nums">01</div>
-            <div className="list-col-grow">
-              <div className="font-bold uppercase">Estudar Programação</div>
-              <div className="text-xs font-semibold opacity-60">
-                Concluir o conteúdo da trilha de formação em SOLID do
-                FullStackClub
+        {tasks.map((task, index) => (
+          <li key={task._id} className="flex items-center gap-4">
+            <button className="btn btn-sm border-2 border-white border-solid rounded-xl mt-2">
+              <PencilLine />
+            </button>
+            <div className="list-row flex-1 bg-emerald-600">
+              <div className="text-4xl font-thin opacity-30 tabular-nums">
+                {String(index + 1).padStart(2, "0")}
+              </div>
+              <div className="list-col-grow">
+                <h1 className="font-bold uppercase">{task.title}</h1>
+                <h3 className="text-xs font-semibold opacity-60">
+                  {task.description}
+                </h3>
+              </div>
+              <div className="mt-2">
+                <input
+                  className="checkbox checkbox-neutral"
+                  type="checkbox"
+                  checked={selectedTask.includes(task._id)}
+                  onChange={() =>
+                    setSelectedTask((current) =>
+                      current.includes(task._id)
+                        ? current.filter((id) => id !== task._id)
+                        : [...current, task._id]
+                    )
+                  }
+                />
               </div>
             </div>
-            <div className="mt-2">
-              <input
-                type="checkbox"
-                defaultChecked
-                className="checkbox checkbox-neutral"
-              />
-            </div>
-          </div>
-        </li>
-
-        <li className="flex items-center gap-4">
-          <button className="btn btn-sm border-2 border-white border-solid rounded-xl mt-2">
-            <PencilLine />
-          </button>
-          <div className="list-row bg-emerald-600">
-            <div className="text-4xl font-thin opacity-30 tabular-nums">01</div>
-            <div className="list-col-grow">
-              <div className="font-bold uppercase">Estudar Programação</div>
-              <div className="text-xs font-semibold opacity-60">
-                Concluir o conteúdo da trilha de formação em SOLID do
-                FullStackClub
-              </div>
-            </div>
-            <div className="mt-2">
-              <input
-                type="checkbox"
-                defaultChecked
-                className="checkbox checkbox-neutral"
-              />
-            </div>
-          </div>
-        </li>
-
-        <li className="flex items-center gap-4">
-          <button className="btn btn-sm border-2 border-white border-solid rounded-xl mt-2">
-            <PencilLine />
-          </button>
-          <div className="list-row bg-emerald-600">
-            <div className="text-4xl font-thin opacity-30 tabular-nums">01</div>
-            <div className="list-col-grow">
-              <div className="font-bold uppercase">Estudar Programação</div>
-              <div className="text-xs font-semibold opacity-60">
-                Concluir o conteúdo da trilha de formação em SOLID do
-                FullStackClub
-              </div>
-            </div>
-            <div className="mt-2">
-              <input
-                type="checkbox"
-                defaultChecked
-                className="checkbox checkbox-neutral"
-              />
-            </div>
-          </div>
-        </li>
-
-        <li className="flex items-center gap-4">
-          <button className="btn btn-sm border-2 border-white border-solid rounded-xl mt-2">
-            <PencilLine />
-          </button>
-          <div className="list-row bg-emerald-600">
-            <div className="text-4xl font-thin opacity-30 tabular-nums">01</div>
-            <div className="list-col-grow">
-              <div className="font-bold uppercase">Estudar Programação</div>
-              <div className="text-xs font-semibold opacity-60">
-                Concluir o conteúdo da trilha de formação em SOLID do
-                FullStackClub
-              </div>
-            </div>
-            <div className="mt-2">
-              <input
-                type="checkbox"
-                defaultChecked
-                className="checkbox checkbox-neutral"
-              />
-            </div>
-          </div>
-        </li>
-
-        <li className="flex items-center gap-4">
-          <button className="btn btn-sm border-2 border-white border-solid rounded-xl mt-2">
-            <PencilLine />
-          </button>
-          <div className="list-row bg-emerald-600">
-            <div className="text-4xl font-thin opacity-30 tabular-nums">01</div>
-            <div className="list-col-grow">
-              <div className="font-bold uppercase">Estudar Programação</div>
-              <div className="text-xs font-semibold opacity-60">
-                Concluir o conteúdo da trilha de formação em SOLID do
-                FullStackClub
-              </div>
-            </div>
-            <div className="mt-2">
-              <input
-                type="checkbox"
-                defaultChecked
-                className="checkbox checkbox-neutral"
-              />
-            </div>
-          </div>
-        </li>
+          </li>
+        ))}
       </ul>
 
       <div className="flex mt-4 justify-center">
-        <button className="btn btn-disabled rounded-md w-[150px] ml-19">
-          Salvar
+        <button
+          disabled={selectedTask.length === 0}
+          className="btn btn-success rounded-md w-[150px] ml-19"
+        >
+          Concluir
         </button>
       </div>
     </div>
