ed9fe51304c954969e46ee15add5adae02f0f6ae
HenriqueCoruqieri <heoliveirac@gmail.com>
2026-06-07 22:03:46 -0300

    feat: ensure user can navigate on breadcrumb menu

--- src/components/taskBook/task-book.jsx
+++ src/components/taskBook/task-book.jsx
@@ -1,24 +1,28 @@
 import { BookOpenText, StickyNotePlus } from "lucide-react"
 import NewTask from "./new-task"
+import TaskList from "./task-list"
+
+import { useState } from "react"
 
 const TaskBook = () => {
+  const [activeView, setActiveView] = useState("new-task")
   return (
     <div className="p-4">
       <div className="breadcrumbs rounded-xl text-emerald-500 font-bold">
         <ul className="w-full justify-center">
           <li className="space-x-2">
             <StickyNotePlus />
-            <a>Nova Tarefa</a>
+            <a onClick={() => setActiveView("new-task")}>Nova Tarefa</a>
           </li>
           <li className="space-x-2">
             <BookOpenText />
-            <a>Tarefas</a>
+            <a onClick={() => setActiveView("tasks")}>Tarefas</a>
           </li>
         </ul>
       </div>
 
       <div className="mt-6">
-        <NewTask />
+        {activeView == "new-task" ? <NewTask /> : <TaskList />}
       </div>
     </div>
   )
